0ab38b2950350d89e36b2b0a2f9a9167925ae64d
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2022-06-15 11:29:58 -0100

    Create the modulo first time

---
+++ dnre-addons/dnre/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class Dnre(http.Controller):
+   @http.route('/dnre/dnre', auth='public')
+   def index(self, **kw):
+      return "Hello, world"
+
+   @http.route('/dnre/dnre/objects', auth='public')
+   def list(self, **kw):
+      return http.request.render('dnre.listing', {
+         'root': '/dnre/dnre',
+         'objects': http.request.env['dnre.dnre'].search([]),
+      })
+
+   @http.route('/dnre/dnre/objects/<model("dnre.dnre"):obj>', auth='public')
+   def object(self, obj, **kw):
+      return http.request.render('dnre.object', {
+         'object': obj
+      })
---
+++ dnre-addons/dnre/models/dga.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+
+class dga(models.Model):
+   _name = 'dnre.dga'
+   _auto = False
+   _description = "DNRE - Recursos Humanos"
+
+   nums = fields.Integer(string="Nº segurado", required=True)
+   ns = fields.Char(string="Nome segurado", required=True)
+   cp = fields.Char(string="CP")
+   pc = fields.Char(string="P/C")
+   ndt = fields.Integer(string="Nº DT")
+   sl = fields.Integer(string="SL")
+   cs = fields.Char(string="CS")
+   cf = fields.Integer(string="CF")
+   cep = fields.Integer(string="CEP")
