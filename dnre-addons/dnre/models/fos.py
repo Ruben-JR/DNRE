93e8cbfae866d7e7ec03d6ea5d97c4a92a52d93e
Ruben-JR <rubenpina758@gmail.com>
2022-07-04 10:42:04 -0100

    Create the tables relations

--- dnre-addons/dnre/models/fos.py
+++ dnre-addons/dnre/models/fos.py
@@ -47,6 +47,19 @@ class dga(models.Model):
     cep = fields.Integer(string="CEP")
     dga_id = fields.Many2one("dnre.fos", string="dga", readonly="True")
 
+    def init(self):
+        self._cr.execute(
+            """
+            CREATE OR REPLACE VIEW dnre_dga AS (
+                SELECT row_number() OVER() as id,
+                so.nums as nums, so.ns as ns, so.cp as cp, so.pc as pc, so.ndt as ndt, so.sl as sl, so.cs as cs, so.cf as cf, so.cep as cep, so.dga.id as dga_ids, so.dgci.id = dgci_ids
+                FROM dnre_fos
+                JOIN dnre_fos ON so.dga_ids = dga.id
+                JOIN dnre_dgci ON so dgci_ids = dgci.id
+            )
+        """
+        )
+
 
 class dgci(models.Model):
     _name = "dnre.dgci"
@@ -62,3 +75,16 @@ class dgci(models.Model):
     cf = fields.Integer(string="CF")
     cep = fields.Integer(string="CEP")
     dgci_id = fields.Many2one("dnre.fos", string="dgci", readonly="True")
+
+    def init(self):
+        self._cr.execute(
+            """
+            CREATE OR REPLACE VIEW dnre_dgci AS (
+                SELECT row_number() OVER() as id,
+                so.nums as nums, so.ns as ns, so.cp as cp, so.pc as pc, so.ndt as ndt, so.sl as sl, so.cs as cs, so.cf as cf, so.cep as cep, so.dga.id as dga_ids, so.dgci.id = dgci_ids
+                FROM dnre_fos
+                JOIN dnre_dga ON so.dga_ids = dga.id
+                JOIN dnre_dgci ON so dgci_ids = dgci.id
+            )
+        """
+        )
