9b710245c9177656fa32f47fc7c5fa37ff8075bf
Ruben-JR <rubenpina758@gmail.com>
2022-07-04 10:04:41 -0100

    Resolving others conflit v1.0

--- dnre-addons/dnre/models/__init__.py
+++ dnre-addons/dnre/models/__init__.py
@@ -1,6 +1,2 @@
-# -*- coding: utf-8 -*-
-
-from . import dga_doc
 from . import dga
-from . import dgci_doc
 from . import dgci
--- dnre-addons/dnre/models/dgci.py
+++ dnre-addons/dnre/models/dgci.py
@@ -1,18 +1,32 @@
-from odoo import models, fields, api
+from odoo import models, fields
+
+
+class dgci_doc(models.Model):
+    _name = "dnre.dgci_doc"
+    _auto = False
+    _description = "DNRE - Recursos Humanos"
+
+    nc = fields.Integer(string="Nº contribuente", required=True)
+    dc = fields.Char(string="Designação contribuente", required=True)
+    ca = fields.Integer(string="CA")
+    da = fields.Char(string="DA")
+    mr = fields.Date(string="MR")
+    tc = fields.Integer(string="TC")
+    r = fields.Char(string="Regime")
+    dgci = fields.One2many("dnre.dgci", string="DGCI lines", readonly="True")
 
 
 class dgci(models.Model):
-   _name = 'dnre.dgci'
-   _auto = False
-   _description = "DNRE - Recursos Humanos"
+    _name = "dnre.dgci"
+    _description = "DGCI Funcionarios"
 
-   nums = fields.Integer(string="Nº segurado", required=True)
-   ns = fields.Char(string="Nome segurado", required=True)
-   cp = fields.Char(string="CP")
-   pc = fields.Char(string="P/C")
-   ndt = fields.Integer(string="Nº DT")
-   sl = fields.Integer(string="SL")
-   cs = fields.Char(string="CS")
-   cf = fields.Integer(string="CF")
-   cep = fields.Integer(string="CEP")
-   
+    dgci_id = fields.Many2one(string="dgci", readonly="True")
+    nums = fields.Integer(string="Nº segurado", required=True)
+    ns = fields.Char(string="Nome segurado", required=True)
+    cp = fields.Char(string="CP")
+    pc = fields.Char(string="P/C")
+    ndt = fields.Integer(string="Nº DT")
+    sl = fields.Integer(string="SL")
+    cs = fields.Char(string="CS")
+    cf = fields.Integer(string="CF")
+    cep = fields.Integer(string="CEP")
